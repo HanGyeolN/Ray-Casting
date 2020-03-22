e1600b8ab31a79ee66f5e0f86abdb904eb499461
Hna <ghnruf@naver.com>
2020-03-23 05:00:43 +0900

    Add | map, ray rendering

--- main.c
+++ main.c
@@ -1,13 +1,36 @@
-#include <unistd.h>
-#include "mlx.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hna <hna@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/23 04:06:30 by hna               #+#    #+#             */
+/*   Updated: 2020/03/23 04:59:58 by hna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-typedef struct	s_window
+#include "raycasting.h"
+#include "stdio.h"
+#include <string.h>
+
+typedef struct		s_map
 {
-	int		width;
-	int		height;
-	void	*win_ptr;
-	void	*mlx_ptr;
-}				t_window;
+	double	width;
+	double	height;
+	char	map[5][7];
+}					t_map;
+
+void	init_map(t_map *map)
+{
+	map->width = 6;
+	map->height = 5;
+	strcpy(map->map[0], "111111");
+	strcpy(map->map[1], "100101");
+	strcpy(map->map[2], "100001");
+	strcpy(map->map[3], "110001");
+	strcpy(map->map[4], "111111");
+}
 
 void	ft_putchar(char c)
 {
@@ -20,17 +43,20 @@ int		deal_key(int key, void *param)
 	return (0);
 }
 
-int		render(void *mlx_ptr, void *win_ptr)
+int		render_map(t_window *window, t_map *map)
 {
-	int		r;
-	int		c;
+	int		x;
+	int		y;
 
-	c = 10;
-	while (++c < 490)
+	x = -1;
+	while (++x < window->width)
 	{
-		r = 10;
-		while (++r < 490)
-			mlx_pixel_put(mlx_ptr, win_ptr, r, c, 0xFFFFFF);
+		y = -1;
+		while (++y < window->height)
+		{
+			if (map->map[(int)(y / (window->height / map->height))][(int)(x / (window->width / map->width))] == '1')
+				mlx_pixel_put(window->mlx_ptr, window->win_ptr, x, y, 0xFFFFFF);
+		}
 	}
 	return (0);
 }
@@ -43,13 +69,32 @@ void	init_window(t_window *window, int width, int height, char *title)
 	window->height = height;
 }
 
+void	init_ray(t_ray *ray)
+{
+	ray->pos_x = 150;
+	ray->pos_y = 150;
+	ray->deg_x = 0;
+	ray->deg_y = 1;
+}
+
+void	render_ray(t_window *window, t_ray *ray)
+{
+	mlx_pixel_put(window->mlx_ptr, window->win_ptr, ray->pos_x, ray->pos_y, 0xFFFFFF);
+}
+
 int		main(void)
 {
 	t_window	*window;
+	t_map		*map;
+	t_ray		ray;
 
-	if (!(window = malloc(sizeof(t_window))))
+	if (!(window = malloc(sizeof(t_window))) || !(map = malloc(sizeof(t_map))))
 		return (0);
-	init_window(window, 360, 200, "title_test");
+	init_window(window, 200, 200, "title_test");
+	init_map(map);
+	init_ray(&ray);
+	render_map(window, map);
+	render_ray(window, &ray);
 	mlx_key_hook(window->win_ptr, deal_key, (void *)0);
 	mlx_loop(window->mlx_ptr);
 }
---
+++ raycasting.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycasting.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hna <hna@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/23 04:04:51 by hna               #+#    #+#             */
+/*   Updated: 2020/03/23 04:37:14 by hna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	RAYCASTING_H
+# define RAYCASTING_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "mlx.h"
+
+typedef struct	s_window
+{
+	double	width;
+	double	height;
+	void	*win_ptr;
+	void	*mlx_ptr;
+}				t_window;
+
+typedef struct	s_ray
+{
+	double	pos_x;
+	double	pos_y;
+	double	deg_x;
+	double	deg_y;
+}				t_ray;
+
+#endif
