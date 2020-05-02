1a65acca1f66cc972bed2d72f977be6095da186b
Hna <ghnruf@naver.com>
2020-05-02 22:44:22 +0900

    Add | Red cross button for exit game

--- srcs/main.c
+++ srcs/main.c
@@ -13,6 +13,25 @@
 #include "raycasting.h"
 #include "libft.h"
 
+int		close_game(t_scene *scene)
+{
+	int		i;
+	ft_printf("Red cross button pushed\n");
+	free(scene->z_buffer);
+	free(scene->sprite);
+	free(scene->player.rays);
+	i = 0;
+	while (i < scene->map.height)
+	{
+		free(scene->map.map_r[i]);
+		i++;
+	}
+	free(scene->map.map_r);
+	mlx_destroy_image(scene->window.mlx_ptr, scene->window.img_ptr);
+	mlx_destroy_window(scene->window.mlx_ptr, scene->window.win_ptr);
+	exit (0);
+}
+
 int		main(void)
 {
 	t_scene		scene;
@@ -23,5 +42,6 @@ int		main(void)
 	ray_casting(&(scene.player.rays), &(scene.map), &scene);
 	render(&scene);
 	mlx_hook((scene.window).win_ptr, 2, 0, press_key, &scene);
+	mlx_hook((scene.window).win_ptr, 17, 0, close_game, &scene);
 	mlx_loop((scene.window).mlx_ptr);
 }
